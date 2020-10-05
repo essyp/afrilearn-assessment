0e36688539d4b668f762d3e66a8d6dc5a97488ce
unknown <fmogbana@gmail.com>
2020-10-05 18:39:23 +0100

    updated package.json

--- config/app.php
+++ config/app.php
@@ -14,8 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    "log"=>"errorlog",
-
+    
     /*
     |--------------------------------------------------------------------------
     | Application Environment
